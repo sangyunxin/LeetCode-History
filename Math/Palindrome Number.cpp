aef4ac080522b5de0e8fc772e0559cdc37fa5b80
sangyunxin <sangyunxin@gmail.com>
2019-06-07 00:11:08 +0800

    美化

--- Math/Palindrome Number.cpp
+++ Math/Palindrome Number.cpp
@@ -1,14 +1,18 @@
-class Solution {
+class Solution
+{
 public:
-    bool isPalindrome(int x) {
-        if(x < 0) return false;
+    bool isPalindrome(int x)
+    {
+        if (x < 0)
+            return false;
         int tmp = x;
         long ret = 0;
-        while(tmp != 0){
+        while (tmp != 0)
+        {
             ret = ret * 10 + (tmp % 10);
             tmp /= 10;
         }
-        if(ret == x)
+        if (ret == x)
             return true;
         return false;
     }
