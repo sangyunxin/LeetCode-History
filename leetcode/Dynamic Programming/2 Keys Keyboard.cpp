6c3101006a3a7385e0e0c5d286a7d30efe315bdc
sangyunxin <sangyunxin@gmail.com>
2019-07-03 22:44:13 +0800

    7月3日打卡

---
+++ leetcode/Dynamic Programming/2 Keys Keyboard.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minSteps(int n) {
+        vector<int> dp(n+1);
+        for(int i = 2; i <= n; ++i){
+            dp[i] = i;
+            for(int j = i - 1; j >= 1; --j){
+                if(i % j == 0)
+                    dp[i] = min(dp[i], dp[j] + (i / j));
+            }
+        }
+        return dp[n];
+    }
+};
